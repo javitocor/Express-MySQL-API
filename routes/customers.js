98678a9a5cf88e5ddab83cc51ff77110c12c5044
javitocor <javitocor@gmail.com>
2021-10-12 00:24:29 +0700

    implement controller

--- routes/customers.js
+++ routes/customers.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+var customer_controller = require('../controllers/customerController');
+
+// Create a new Customer
+router.post("/customers", customer_controller.create);
+
+// Retrieve all Customers
+router.get("/customers", customer_controller.findAll);
+
+// Retrieve a single Customer with customerId
+router.get("/customers/:customerId", customer_controller.findOne);
+
+// Update a Customer with customerId
+router.put("/customers/:customerId", customer_controller.update);
+
+// Delete a Customer with customerId
+router.delete("/customers/:customerId", customer_controller.delete);
+
+// Delete Customer
+router.delete("/customers", customer_controller.deleteAll);
 
 module.exports = router;
